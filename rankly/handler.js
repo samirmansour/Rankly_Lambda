22e81dfea4cbdb670d5b50a0e8a33c413591ccb7
Sammy Mansour <42918370+samirmansour@users.noreply.github.com>
2019-02-27 07:47:13 -0500

    Add files via upload

---
+++ rankly/handler.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const emojis = ['🙌', '👍', '👨‍👨‍👦', '🥉', '🏆'];
+
+module.exports.rank = async (event, context) => {
+  const rank = event.queryStringParameters.rank
+  const rankEmoji = emojis[rank > emojis.length ? emojis.length - 1 : rank];
+  return {
+    statusCode: 200,
+    headers: {
+    	'Access-Control-Allow-Origin' : '*'
+    },
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: rankEmoji,
+    }),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
